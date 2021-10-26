6d4040878ea4831b0f936f5576f210c8ab815efb
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2021-10-25 22:12:55 -0500

    :bento: 159. Componentes EntryList y Entry

--- src/modules/daybook/components/Entrylist.vue
+++ src/modules/daybook/components/Entrylist.vue
@@ -1,10 +1,30 @@
 <template>
-  <h1>Entrylist</h1>
+  <div class="entry-list-container">
+    <div class="px-2 pt-2">
+      <input type="text" class="form-control" placeholder="Buscar" />
+    </div>
+
+    <div class="entry-scrollarea">
+      <Entry v-for="item in 100" :key="item" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { defineAsyncComponent } from "@vue/runtime-core";
+
+export default {
+  components: { Entry: defineAsyncComponent(() => import("./Entry.vue")) },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.entry-list-container {
+  border-right: 1px solid #2c3e50;
+  height: calc(100vh - 56px);
+}
+.entry-scrollarea {
+  height: calc(100vh - 115px);
+  overflow: scroll;
+}
 </style>
