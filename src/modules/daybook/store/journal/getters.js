c116a4db2c9fa13e5097c0f6004435ccfcfe9280
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2021-10-27 10:49:16 -0500

    :bento: 168. GetEntryById - Obtener una entrada por el id

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -10,4 +10,8 @@ export const getEntryByTerm = (state) => (term = '') => {
 
   return state.entries.filter(entry => entry.text.toLowerCase().includes(term.toLowerCase()));
 };
-export const getEntryById = (/*state*/) => {};
+export const getEntryById = (state) => (id = '') => {
+  const entry = state.entries.find(entry => entry.id === id);
+  if(!entry) return;
+  return entry;
+};
--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -32,11 +32,30 @@
 
 <script>
 import { defineAsyncComponent } from "@vue/runtime-core";
+import {mapGetters} from 'vuex';
 
 export default {
+  props:{
+    id: {
+      type: String,
+      required: true
+    }
+  },
   components: {
     Fab: defineAsyncComponent(() => import("../components/Fab.vue")),
   },
+  computed: {
+    ...mapGetters('journal', ['getEntryById'])
+  },
+  methods:{
+    loadEntry(){
+      const entry = this.getEntryById(this.id);
+      console.log(entry)
+    }
+  },
+  created() {
+    this.loadEntry();
+  }
 };
 </script>
 
