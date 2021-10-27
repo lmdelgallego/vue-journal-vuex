3d7550dbc048a4d24e6937a4dcdf0d19bcbf31b7
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2021-10-27 09:27:45 -0500

    :bento: 166. 167. EntryComponent - Información al componente

--- src/modules/daybook/components/Entrylist.vue
+++ src/modules/daybook/components/Entrylist.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="entry-scrollarea">
-      <Entry v-for="item in entriesByTerm" :key="item" />
+      <Entry v-for="entry in entriesByTerm" :key="entry.id" :entry="entry" />
     </div>
   </div>
 </template>
