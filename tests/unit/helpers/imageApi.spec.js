8b628eea5fda5539da375779fb987224e383a01c
Luis Miguel <lmdelgallego@gmail.com>
2021-11-12 15:01:47 -0500

    :bento: 202. Borrar archivos subidos en las pruebas

--- tests/unit/helpers/imageApi.spec.js
+++ tests/unit/helpers/imageApi.spec.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 import imageApi from '@/helpers/imageApi';
+import cloudinary from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: 'alucardluis',
+  api_key: '676364253242874',
+  api_secret: '2nFml2s1kILek2WOg1cEWT-uJZ4'
+})
 
 describe('ImageApi', () => {
-  test('should be upload image and return a url', async () => {
+  test('should be upload image and return a url', async (done) => {
     const {data} = await axios.get('https://res.cloudinary.com/alucardluis/image/upload/v1612270415/x8tmirlsesb5pizhdzbq.jpg', {
       responseType: 'arraybuffer'
     });
@@ -17,7 +24,13 @@ describe('ImageApi', () => {
     expect(url).toEqual({
       picture: expect.any(String),
       picture_id: expect.any(String)
-    })
+    });
+
+    const segments = url.picture.split('/');
+    const imageId = segments[segments.length - 1].replace('.jpg', '');
+
+
+    cloudinary.v2.api.delete_resources(imageId, {}, () => done());
 
   });
 })
