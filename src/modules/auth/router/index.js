8d660d7f59a3bc8105b84790136abad3dddc51da
Luis Miguel <lmdelgallego@gmail.com>
2021-12-04 11:35:40 -0500

    :bento: 261. Rutas de Login y Register

--- src/modules/auth/router/index.js
+++ src/modules/auth/router/index.js
@@ -1,5 +1,16 @@
 export default {
   name: 'auth',
   component: () => import( /* webpackChunkName: "auth-layout" */ '@/modules/auth/layouts/AuthLayout.vue'),
-  children: []
+  children: [
+    {
+      name: 'login',
+      path: '',
+      component: () => import( /* webpackChunkName: "login" */ '@/modules/auth/views/Login.vue'),
+    },
+    {
+      name: 'register',
+      path: '/register',
+      component: () => import( /* webpackChunkName: "register" */ '@/modules/auth/views/Register.vue'),
+    },
+  ]
 }
