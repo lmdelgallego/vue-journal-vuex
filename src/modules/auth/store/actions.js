861b3227cd394714b0ba88ebfbd4755fc1a9b59d
Luis Miguel <lmdelgallego@gmail.com>
2021-12-06 11:43:54 -0500

    :bento: 266. Mutation - LoginUser

--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -4,13 +4,12 @@ export const createUser = async ({commit}, user) => {
   try {
     const { name, email, password } = user;
     const {data} = await authApi.post(":signUp", { email, password, returnSecureToken: true });
-    const { idToken } = data;
+    const { idToken, refreshToken } = data;
 
     const respUpdateName = await authApi.post(":update", { displayName: name, idToken });
     console.log(commit, respUpdateName);
-    //TODO: mutation loginUser
-    //commit('setUser', response.data, name);
-
+    delete user.password;
+    commit('loginUser', {user, idToken, refreshToken});
 
     return {ok: true, message: "User created", token: data.idToken};
   } catch ({response}) {
--- src/modules/auth/store/mutations.js
+++ src/modules/auth/store/mutations.js
@@ -1,5 +1,14 @@
-/*
-export const myMutation = (state) => {
+export const loginUser = (state, { user, idToken, refreshToken}) => {
+  if (idToken) {
+    localStorage.setItem('idToken', idToken);
+    state.idToken = idToken;
+  }
+  if (refreshToken) {
+    localStorage.setItem('refreshToken', refreshToken);
+    state.refreshToken = refreshToken;
+  }
+
+  state.user = user;
+  state.status = 'authenticated';
 
 }
-*/
