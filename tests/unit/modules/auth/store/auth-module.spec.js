d84c6207b0a8ef64c7b9499b0ac9485c0fe449dc
Luis Miguel <lmdelgallego@gmail.com>
2021-12-10 10:19:54 -0500

    :bento: 282. Mutations - logout

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -11,8 +11,55 @@ describe('Vuex: auth module', () => {
 
     const { status, user, idToken, refreshToken } = store.state.auth;
     expect(status).toBe('authenticating');
-    expect(user).toBe(null);
-    expect(idToken).toBe(null);
-    expect(refreshToken).toBe(null);
+    expect(user).toBeNull();
+    expect(idToken).toBeNull();
+    expect(refreshToken).toBeNull();
+  });
+
+  describe('#MUTATIONS', () => {
+    test('loginUser', () => {
+      const store = createVuexStore({
+        status: 'authenticating',
+        user: null,
+        idToken: null,
+        refreshToken: null,
+      });
+
+      const payload = {
+        user: { name: 'Luis Miguel', email: 'luismiguel@gmail.com' },
+        idToken: 'ABC-123',
+        refreshToken: 'ABC-123',
+      };
+
+      store.commit('auth/loginUser', payload);
+
+      const { status, user, idToken, refreshToken } = store.state.auth;
+      expect(status).toBe('authenticated');
+      expect(user).toEqual(payload.user);
+      expect(idToken).toBe('ABC-123');
+      expect(refreshToken).toBe('ABC-123');
+    });
+
+    test('logoutUser', () => {
+      localStorage.setItem('idToken', 'ABC-123');
+      localStorage.setItem('refreshToken', 'ABC-123');
+
+      const store = createVuexStore({
+        status: 'authenticated',
+        user: { name: 'Luis Miguel', email: 'luismiguel@gmail.com' },
+        idToken: 'ABC-123',
+        refreshToken: 'ABC-123',
+      });
+
+      store.commit('auth/logoutUser');
+      const { status, user, idToken, refreshToken } = store.state.auth;
+      expect(status).toBe('not-authenticated');
+      expect(user).toBeNull();
+      expect(idToken).toBeNull();
+      expect(refreshToken).toBeNull();
+
+      expect(localStorage.getItem('idToken')).toBeNull();
+      expect(localStorage.getItem('refreshToken')).toBeNull();
+    });
   });
 });
